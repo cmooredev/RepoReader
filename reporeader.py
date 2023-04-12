6f14b1eeb3bc9025ea7c66127c8a7d898379b236
cmooredev <cmooredev@pm.me>
2023-04-12 00:11:14 -0700

    less tokens, more relevant answers

--- reporeader.py
+++ reporeader.py
@@ -5,23 +5,38 @@ import subprocess
 import tempfile
 from rank_bm25 import BM25Okapi
 from dotenv import load_dotenv
-from langchain.document_loaders import DirectoryLoader, TextLoader, NotebookLoader
+from langchain.document_loaders import DirectoryLoader, NotebookLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 from langchain.llms import OpenAI
 from langchain import PromptTemplate, LLMChain
+import re
+import nltk
+
+nltk.download("punkt")
 
 WHITE = "\033[37m"
 GREEN = "\033[32m"
 RESET_COLOR = "\033[0m"
 
+model_name = "gpt-3.5-turbo"
+
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
+def clean_and_tokenize(text):
+    text = re.sub(r'\s+', ' ', text)
+    text = re.sub(r'<[^>]*>', '', text)
+    text = re.sub(r'\[.*?\]', '', text)
+    text = re.sub(r'\(.*?\)', '', text)
+    text = re.sub(r'\b(?:http|ftp)s?://\S+', '', text)
+    text = re.sub(r'\W', ' ', text)
+    text = re.sub(r'\d+', '', text)
+    text = text.lower()
+    return nltk.word_tokenize(text)
+
 def format_documents(documents):
-    numbered_docs = "\n".join([f"{i+1}. {doc.metadata['source']}: {doc.page_content}" for i, doc in enumerate(documents)])
+    numbered_docs = "\n".join([f"{i+1}. {os.path.basename(doc.metadata['source'])}: {doc.page_content}" for i, doc in enumerate(documents)])
     return numbered_docs
 
 def clone_github_repo(github_url, local_path):
@@ -36,29 +51,21 @@ def load_and_index_files(repo_path):
     extensions = ['txt', 'md', 'markdown', 'rst', 'py', 'js', 'java', 'c', 'cpp', 'cs', 'go', 'rb', 'php', 'scala', 'html', 'htm', 'xml', 'json', 'yaml', 'yml', 'ini', 'toml', 'cfg', 'conf', 'sh', 'bash', 'css', 'scss', 'sql', 'gitignore', 'dockerignore', 'editorconfig', 'ipynb']
 
     file_type_counts = {}
-    filenames = []
-    # Use a dictionary to store documents with their respective file_ids as keys
     documents_dict = {}
     documents = []
+    filenames = []
+
     for ext in extensions:
         glob_pattern = f'**/*.{ext}'
         try:
+            loader = None
             if ext == 'ipynb':
-                # Load .ipynb files using NotebookLoader
-                print(f"Loading .ipynb files with pattern '{glob_pattern}'")
-                notebook_documents = []
-                for notebook_path in Path(repo_path).glob(glob_pattern):
-                    print(f"Processing .ipynb file: {notebook_path}")
-                    loader = NotebookLoader(str(notebook_path), include_outputs=True, max_output_length=20, remove_newline=True)
-                    for doc in loader.load():
-                        notebook_documents.append(doc)
-                print(f"Loaded {len(notebook_documents)} .ipynb documents")
-                documents += notebook_documents
+                loader = NotebookLoader(str(notebook_path), include_outputs=True, max_output_length=20, remove_newline=True)
             else:
                 loader = DirectoryLoader(repo_path, glob=glob_pattern)
-                loaded_documents = loader.load() if callable(loader.load) else []
-                print(f"Loaded {len(loaded_documents)} documents with pattern '{glob_pattern}'")
-                documents += loaded_documents
+
+            loaded_documents = loader.load() if callable(loader.load) else []
+            documents += loaded_documents
 
             if documents:
                 file_type_counts[ext] = len(documents)
@@ -68,7 +75,7 @@ def load_and_index_files(repo_path):
                 file_id = str(uuid.uuid4())  # Generate a unique file identifier as a string
                 doc.metadata['source'] = relative_path
                 doc.metadata['file_id'] = file_id  # Add the file identifier to the metadata
-                filenames.append(relative_path)  # Add the filename to the list
+                filenames.append(relative_path) 
 
                 # Add the document to the dictionary using the file_id as the key
                 documents_dict[file_id] = doc
@@ -76,12 +83,10 @@ def load_and_index_files(repo_path):
             print(f"Error loading files with pattern '{glob_pattern}': {e}")
             continue
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=100)
 
-    # Split each document in the dictionary and store them in a new dictionary with the same file_ids
     split_documents_dict = {}
     for file_id, original_doc in documents_dict.items():
-        print(f"Processing file_id: {file_id}")  # Add this print statement
         split_docs = text_splitter.split_documents([original_doc])
         for split_doc in split_docs:
             file_id = split_doc.metadata['file_id']
@@ -90,41 +95,43 @@ def load_and_index_files(repo_path):
 
         split_documents_dict[file_id] = split_docs
 
-
-    # Flatten the split_documents_dict into a list
     documents = [split_doc for split_docs in split_documents_dict.values() for split_doc in split_docs]
 
-    # Add the file_id metadata field to the documents after splitting
     for i, (split_doc, original_doc) in enumerate(zip(documents, documents_dict.values())):
         split_doc.metadata['file_id'] = original_doc.metadata['file_id']
 
     embeddings = OpenAIEmbeddings()
     index = None
     if documents:
-        tokenized_documents = [doc.page_content.split(" ") for doc in documents]
+        tokenized_documents = [clean_and_tokenize(doc.page_content) for doc in documents]
         index = BM25Okapi(tokenized_documents)
-    return index, documents, file_type_counts, filenames  # Updated the return statement to include filenames
-
+    return index, documents, file_type_counts, filenames
 
 def ask_question(documents, question, llm_chain, repo_name, github_url, conversation_history, file_type_counts, filenames):
-    tokenized_question = question.split(" ")
+    tokenized_question = clean_and_tokenize(question)
     scores = index.get_scores(tokenized_question)
-    top_k_indices = sorted(range(len(scores)), key=lambda i: scores[i], reverse=True)[:4]  # Get the indices of the top 4 documents
+    top_k_indices = sorted(range(len(scores)), key=lambda i: scores[i], reverse=True)[:5]
     similar_docs = [documents[i] for i in top_k_indices]
 
-    numbered_documents = format_documents(similar_docs)
-    file_type_summary = ", ".join([f"{count} {ext} files" for ext, count in file_type_counts.items()])
-    file_name_summary = ", ".join([f"{i+1}. {filename}" for i, filename in enumerate(filenames)])
-    context = f"This question is about the GitHub repository '{repo_name}' available at {github_url}. The repository contains {file_type_summary}. Some files are split into smaller pieces for better processing. The actual files in the repository are: {file_name_summary}"
+    relevant_docs = [doc for doc in similar_docs if len(set(tokenized_question).intersection(clean_and_tokenize(doc.page_content))) >= 3]
+
+    numbered_documents = format_documents(relevant_docs)
+    context = f"This question is about the GitHub repository '{repo_name}' available at {github_url}. The most relevant documents are:\n\n{numbered_documents}"
+
     answer_with_sources = llm_chain.run(
+        model=model_name,
         question=question,
         context=context,
-        numbered_documents=numbered_documents,
         repo_name=repo_name,
         github_url=github_url,
-        conversation_history=conversation_history
+        conversation_history=conversation_history,
+        numbered_documents=numbered_documents
     )
     return answer_with_sources
+
+def format_user_question(question):
+    question = re.sub(r'\s+', ' ', question).strip()
+    return question
 
 if __name__ == "__main__":
     github_url = input("Enter the GitHub URL of the repository: ")
@@ -133,48 +140,33 @@ if __name__ == "__main__":
     print("Cloning the repository...")
     with tempfile.TemporaryDirectory() as local_path:
         if clone_github_repo(github_url, local_path):
-            index, documents, file_type_counts, filenames = load_and_index_files(local_path)  # Updated the function call to receive filenames
-
+            index, documents, file_type_counts, filenames = load_and_index_files(local_path)
             if index is None:
                 print("No documents were found to index. Exiting.")
                 exit()
 
             print("Repository cloned. Indexing files...")
             llm = OpenAI(api_key=OPENAI_API_KEY, temperature=0)
+            
             template = """
-                Conversation History:
-                {conversation_history}
-                Current Question: {question}
+            Repo: {repo_name} ({github_url}) | Conv: {conversation_history} | Docs: {numbered_documents} | Q: {question}
 
-                Repository Details:
-                - Name: {repo_name}
-                - URL: {github_url}
+            Instr:
+            1. Answer based on context/docs.
+            2. Focus on repo/code.
+            3. Consider:
+            a. Purpose/features - describe.
+            b. Functions/code - provide details/samples.
+            c. Setup/usage - give instructions.
+            d. List files - share relevant names.
+            4. Unsure? Say "I am not sure".
 
-                Context: {context}
-
-                Codebase Information:
-                {numbered_documents}
-
-                Instructions: 
-                - Provide a detailed, accurate, and relevant answer to the current question based on the provided documents and context.
-                - If the answer requires code snippets, include them in your response.
-                - When referring to specific files or sections in the documents, mention them by their number (e.g., "In document 2, ...").
-                - If the answer requires a step-by-step explanation or a list, use bullet points or numbered lists.
-                - Use the conversation history for context regarding previous questions and answers.
-                - If you cannot find a relevant answer, reply with 'I am not sure'.
-                - Do not make up any information; only work with the context and documents provided.
-                - Only include information relevant to the question.  Don't add extra unnecessary information.
-                - MANDATORY: Follow instructions exactly as they are written above.
-
-                Answer Format:
-                - Start your answer with "Answer:" followed by your response.
-                - If your response has multiple parts, separate them with a newline.
-
-                Answer:
+            Answer: 
             """
+
             prompt = PromptTemplate(
                 template=template,
-                input_variables=["conversation_history", "question", "context", "numbered_documents", "repo_name", "github_url"]
+                input_variables=["repo_name", "github_url", "conversation_history", "question", "numbered_documents"]
             )
 
             llm_chain = LLMChain(prompt=prompt, llm=llm)
@@ -185,14 +177,15 @@ if __name__ == "__main__":
                     user_question = input("\n" + WHITE + "Ask a question about the repository (type 'exit()' to quit): " + RESET_COLOR)
                     if user_question.lower() == "exit()":
                         break
-                    
+
+                    user_question = format_user_question(user_question)
+
                     answer = ask_question(documents, user_question, llm_chain, repo_name, github_url, conversation_history, file_type_counts, filenames)
                     print(GREEN + '\nANSWER\n' + answer + RESET_COLOR + '\n')
                     conversation_history += f"Question: {user_question}\nAnswer: {answer}\n"
                 except Exception as e:
                     print(f"An error occurred: {e}")
                     break
-
 
         else:
             print("Failed to clone the repository.")
