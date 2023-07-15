7886b408f1d5388d5ac39af44f0dab827e554e33
Eilon Eilstein <eeilstein@lightricks.com>
2023-07-15 13:32:49 +0300

    implement in a single notebook

--- app.py
+++
@@ -1,5 +0,0 @@
-# app.py
-from main import main
-
-if __name__ == "__main__":
-    main()
--- config.py
+++
@@ -1,5 +0,0 @@
-#config.py
-WHITE = "\033[37m"
-GREEN = "\033[32m"
-RESET_COLOR = "\033[0m"
-model_name = "gpt-4"
